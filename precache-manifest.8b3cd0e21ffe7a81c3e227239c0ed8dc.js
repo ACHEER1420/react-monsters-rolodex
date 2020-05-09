7e7aa1b0b451794f453faa327121d825ae29a80f
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2020-05-08 20:58:45 -0500

    Updates

---
+++ precache-manifest.8b3cd0e21ffe7a81c3e227239c0ed8dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5309912bb725a01ac0d54024035d92f",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c765d50496a8b2ede820",
+    "url": "/react-monsters-rolodex/static/css/main.5b3eeaa5.chunk.css"
+  },
+  {
+    "revision": "cae8900dcf0593a9e8f0",
+    "url": "/react-monsters-rolodex/static/js/2.975f868f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.975f868f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c765d50496a8b2ede820",
+    "url": "/react-monsters-rolodex/static/js/main.b09f13e2.chunk.js"
+  },
+  {
+    "revision": "5d1bcaa8ffa01b561da5",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.ab59da59.js"
+  }
+]);
